7f9ece3e6c75ea3db58b877b07aaa7c91e7d336c
Jose Lorenzo <jolorenzom@gmail.com>
2018-12-02 17:53:57 +0100

    - Create user (no Admin). - Read ONE user.

---
+++ src/script/create_user_general.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * PHP version 7.2
+ * src\create_user_admin.php
+ *
+ * @category Utils
+ * @package  MiW\Results
+ * @author   Javier Gil <franciscojavier.gil@upm.es>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://www.etsisi.upm.es ETS de Ingeniería de Sistemas Informáticos
+ */
+
+use MiW\Results\Entity\User;
+use MiW\Results\Utils;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+// Carga las variables de entorno
+$dotenv = new \Dotenv\Dotenv(
+    __DIR__ . '/../..',
+    Utils::getEnvFileName(__DIR__ . '/../..')
+);
+$dotenv->load();
+
+$entityManager = Utils::getEntityManager();
+
+if ($argc !== 4) {
+    $fich = basename(__FILE__);
+    echo <<< MARCA_FIN
+
+    Usage: $fich <Username> <Email> <Password>
+
+MARCA_FIN;
+    exit(0);
+}
+
+echo "ARG_C: ".$argc;
+
+echo " ARG_0: ".$argv[0];
+echo " ARG_1: ".$argv[1];
+echo " ARG_2: ".$argv[2];
+echo " ARG_3: ".$argv[3]." ";
+
+$newUsername = $argv[1];
+$newUserEmail = $argv[2];
+$newUserPassword = $argv[3];
+
+$user = new User();
+$user->setUsername($newUsername);
+$user->setEmail($newUserEmail);
+$user->setPassword($newUserPassword);
+$user->setEnabled(true);
+$user->setIsAdmin(false);
+
+try {
+    $entityManager->persist($user);
+    $entityManager->flush();
+    echo 'Created User with ID #' . $user->getId() . PHP_EOL;
+} catch (Exception $exception) {
+    echo $exception->getMessage() . PHP_EOL;
+}
